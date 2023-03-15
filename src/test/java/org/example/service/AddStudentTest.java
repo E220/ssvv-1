95c0fb57a7b626f7f7ba88af9c25460f1cd978d4
Tiha <tgtihamer@yahoo.com>
2023-03-15 15:31:52 +0200

    fix tests

--- src/test/java/org/example/service/AddStudentTest.java
+++ src/test/java/org/example/service/AddStudentTest.java
@@ -7,19 +7,25 @@ import org.example.validation.ValidationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddStudentTest {
     private Service service;
     @BeforeEach
-    void setupRepository() {
+    void setupServiceAndEmptyRepository() {
         String filenameStudent = "fisiere/test/Studenti.xml";
         StudentXMLRepo repo = new StudentXMLRepo(filenameStudent);
+        List<Student> list = new ArrayList<>();
+        repo.findAll().forEach(list::add);
+        list.forEach(student -> repo.delete(student.getID()));
         StudentValidator validator = new StudentValidator();
         this.service = new Service(repo, validator, null, null, null, null);
     }
     @Test
-    void groupNumberPositive_shouldPass() {
+    void groupNumberPositive_shouldReturnNull() {
         Student student = new Student("test1", "Sanyi", 1, "sanyi@gmail.com");
         assertNull(this.service.addStudent(student));
     }
